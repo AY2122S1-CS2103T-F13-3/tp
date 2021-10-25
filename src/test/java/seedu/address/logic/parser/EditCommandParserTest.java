438880ca531bb1bd2c2460b38026787f9f91b3a3
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2021-10-25 14:11:13 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -2,9 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.ACAD_LEVEL_DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.ACAD_LEVEL_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.ACAD_STREAM_DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.ACAD_STREAM_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
